e06d9a0a44a7d440685991064997d89af4b481ce
mori <mori@gum.tokyo.jp>
2014-09-27 00:02:49 +0900

    First Commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GraphDemo::Application.config.secret_key_base = 'b36eab888375c6acc39c20b3e689813919d7e21d7ea1da7d049c75780a53c9d0e5b92546699b18ade01a154398feb4d7db337c85a29ac7d17ee6033576a2c4ef'
---
+++ vendor/bundle/ruby/2.0.0/gems/railties-4.0.10/lib/rails/version.rb
@@ -0,0 +1,10 @@
+module Rails
+  module VERSION
+    MAJOR = 4
+    MINOR = 0
+    TINY  = 10
+    PRE   = nil
+
+    STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
+  end
+end
